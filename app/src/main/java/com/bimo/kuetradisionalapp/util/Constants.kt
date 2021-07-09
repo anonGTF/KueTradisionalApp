c663a03a076ae89d018dc40b02669be574063f96
anonGTF <anongtf26@gmail.com>
2021-07-09 19:44:19 +0700

    fix: change threshold to 73%

--- app/src/main/java/com/bimo/kuetradisionalapp/util/Constants.kt
+++ app/src/main/java/com/bimo/kuetradisionalapp/util/Constants.kt
@@ -2,5 +2,5 @@ package com.bimo.kuetradisionalapp.util
 
 const val BASE_URL = "http://bimoprasetyoafif.engineer/"
 const val YOUTUBE_API_KEY = "AIzaSyDVQWxtDu4AVx5GF4Alqr5zmRyV_f5T40s"
-const val THRESHOLD = 0.6
+const val THRESHOLD = 0.73
 const val NO_INTERNET_ERROR = "Unable to resolve host \"bimoprasetyoafif.engineer\": No address associated with hostname"
